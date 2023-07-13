d4ee1057fc0d22a447507478ef5f1c507d6870a8
Antonio Toriz <torizantonio15@gmail.com>
2023-07-13 01:09:18 -0700

    updated DbContext to include a User db table

--- Data/MyContext.cs
+++ Data/MyContext.cs
@@ -1,9 +1,11 @@
+using el_bazar.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace el_bazar.Data
 {
     public class MyContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "server=localhost;user=root;database=el-bazar";
